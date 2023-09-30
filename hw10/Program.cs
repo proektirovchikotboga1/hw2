efe68c6c0d3ac4d96ac5f2c5b499ff3fbacda3a7
Dmitry <d.cheryachukin@gmail.com>
2023-09-30 13:25:03 +0700

    added homework task

---
+++ hw10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число N: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+if (num.ToString().Length != 3)
+{
+    Console.Write("Введено некорректное число N: ");
+    return;
+}
+
+char secondDigit = num.ToString()[1];
+Console.WriteLine($"Вторая цифра числа {num} равна {secondDigit}");

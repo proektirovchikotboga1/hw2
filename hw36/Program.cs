336bf3a1df2980097d847c81258ce9029cb11f74
Dmitry <d.cheryachukin@gmail.com>
2023-10-08 13:32:51 +0700

    added new homework task

---
+++ hw36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// создаем массив и заполняем случайными числами
+        int[] array = new int[4];
+
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-100, 100);
+        }
+
+        // выводим исходный массив на экран
+        Console.WriteLine("Исходный массив:");
+        foreach (var element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+
+        // находим сумму элементов на нечетных позициях
+        int sum = 0;
+        for (int i = 1; i < array.Length; i += 2)
+        {
+            sum += array[i];
+        }
+
+        // выводим сумму на экран
+        Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
+
+        Console.ReadLine();
---
+++ hw38/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] numbers = { 3.22, 4.2, 1.15, 77.15, 65.2 };
+
+        double min = numbers[0];
+        double max = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min)
+                min = numbers[i];
+
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+
+        double difference = max - min;
+        Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {difference}");

354d84a3252de3ba705c1956baae1ef2d909239c
Dmitry <d.cheryachukin@gmail.com>
2023-10-22 14:15:24 +0700

    Added homework task

---
+++ hw66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+    static int SumNaturalNumbers(int m, int n)
+    {
+        if (m > n)
+        {
+            return 0;
+        }
+        else
+        {
+            return m + SumNaturalNumbers(m+1, n);
+        }
+    }
+ 
+        Console.Write("Введите значение M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        int sum = SumNaturalNumbers(m, n);
+        
+        Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {sum}.");
+   
+
---
+++ hw68/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+
+        Console.WriteLine("Введите значение для m:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+        Console.WriteLine("A({0}, {1}) = {2}", m, n, result);
+    
+
+
+
+
+
+
